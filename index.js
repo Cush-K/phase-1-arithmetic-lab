af367090103b4074a023d58c678696fa133c869d
Kalvine <kkalvine4@gmail.com>
2024-06-26 00:57:32 +0300

    Adds Arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1*num2;
+    console.log(`multiply`);
+
+const random = Math.floor(Math.random() * 10) + 1;
+    console.log(`random`);
+
+const num3 = 9;
+const num4 = 5;
+
+const mod = num3%num4;
+    console.log(`mod`);
+
+const max = Math.max(1,5,10,15,20);
